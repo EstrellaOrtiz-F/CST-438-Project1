06786b1e3f919ed19f9d8d448b8c0f5ab34463e2
EstrellaOrtiz-F <eortiz-felix@csumb.edu>
2026-01-28 01:13:46 -0800

    Added the API implementation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
     // Room (basic setup)
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
