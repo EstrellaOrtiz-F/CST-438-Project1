4d8e72304ce5fc9e0dcf567660a7c8b915670720
EstrellaOrtiz-F <eortiz-felix@csumb.edu>
2026-01-28 00:21:26 -0800

    Created AppDatabase

---
+++ app/src/main/java/com/example/project1/Database/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.project1.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [UserEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDAO
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_db"
+                ).build()
+            }
+            return INSTANCE!!
+        }
+    }
+
+}
