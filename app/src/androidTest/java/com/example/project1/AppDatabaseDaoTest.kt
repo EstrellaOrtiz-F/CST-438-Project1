5090f2d401a0108d23f763f9669611bcebca463f
EstrellaOrtiz-F <eortiz-felix@csumb.edu>
2026-02-16 12:22:13 -0800

    Created and started Database test

---
+++ app/src/androidTest/java/com/example/project1/AppDatabaseDaoTest.kt
@@ -0,0 +1,18 @@
+package com.example.project1
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+
+/**
+ * Author: Estrella Ortiz
+ * Date: 2/16/26
+ */
+
+@RunWith(AndroidJUnit4::class)
+class AppDatabaseDaoTest {
+
+
+
+
+}
